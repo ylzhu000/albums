782dad4479280191181a81d7fa5aa2171ad53a0c
ylzhu000 <yzhu000@citymail.cuny.edu>
2019-01-07 23:29:33 -0500

    first commint

---
+++ index.js
@@ -0,0 +1,12 @@
+/** @format */
+
+import React from 'react';
+import { Text, AppRegistry } from 'react-native';
+
+
+const App = () => (
+	<Text>Hello World!</Text>
+);
+
+AppRegistry.registerComponent('Albums', () => App);
+
